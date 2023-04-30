7b4b09d985222e9f6e5e6f5124d3f176c0371f85
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2023-04-30 23:50:47 +0300

    Update result.php

--- result.php
+++ result.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" 
-    integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>pHp</title>
-</head>
-<body>
-
 <?php
-$email = isset($_POST['yourEmail']) ? $_POST['yourEmail'] : "";
-$password = isset($_POST['password']) ? $_POST['password'] : "";
-
-echo "Hoşgeldiniz, E-Posta Adresiniz: " . $email . ".<br>";
+  // Verileri Ekrana Yazdırma
+    echo "Adı: " . $_POST["personal_fname"] . "<br>";
+    echo "Soyadı: " . $_POST["personal_lname"]. "<br>";
+    echo "Adresi: " . $_POST["personal_address"]. "<br>";
+    echo "Medeni Hali: " . $_POST["medeni_hal"]. "<br>";
+    echo "Kullanıcı: " . $_POST["username"]. "<br>";
+    echo "Parola: " . $_POST["password"]. "<br>";
+   
+    echo "İlaç Tedavisi: " . $_POST["medication_now"]. "<br>";
+    echo "İlaçlar: " . $_POST["kullandigi_ilaclar"];
 
 
 ?>
-
-</body>
-</html>
